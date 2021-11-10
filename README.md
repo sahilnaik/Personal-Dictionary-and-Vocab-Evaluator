f083c9c5ddec8152d95cd35e6f958d1517733cfa
Vatsal <vapatwa2004@gmail.com>
2021-11-10 18:18:28 -0500

    deleted everything

--- README.md
+++
@@ -1,2 +0,0 @@
-# Personal-Dictionary-and-Vocab-Evaluator
-This is the Final Project Repository for CS 546-A course at Stevens Institute of Technology
--- public/js/signup.js
+++
@@ -1,8 +0,0 @@
-function signup() {
-  let password = document.getElementById("password").value;
-  let email = document.getElementById("email").value;
-  let firstName = document.getElementById("firstName").value;
-  let lastName = document.getElementById("lastName").value;
-  let phone = document.getElementById("phone").value;
-  let repeatPassword = document.getElementById("password-repeat").value;
-}
