3f721274c50f9e884e4ea62adf69ceae6e4b611d
sahilnaik <sahil.j.naik@gmail.com>
2021-11-09 20:26:46 -0500

    User database, MCQ database and initial seed. Signup html page

--- app.js
+++ app.js
@@ -2,3 +2,23 @@ const connection = require("./config/mongoConnection");
 const user = require("./data/user");
 const mcq = require("./data/mcq");
 const word = require("./data/words");
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use("/public", express.static(__dirname + "/public"));
+app.set("views", express.static(__dirname + "/views"));
+
+app.get("/", function (request, response) {
+  response.sendFile(path.resolve("views/users/signup.html"));
+});
+
+app.use("*", (request, response) => {
+  response.status(404).json({ error: "Route not found" });
+});
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
+});
