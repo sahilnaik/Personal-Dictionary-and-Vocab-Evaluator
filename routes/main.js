17d67bd1190519f9cad33df426ddceac9bdcaf20
govindr15 <govindr15895@gmail.com>
2021-11-26 13:48:06 -0500

    updating alignment

--- routes/main.js
+++ routes/main.js
@@ -5,8 +5,12 @@ const path = require('path');
 
 router.get('/', async(req,res)=>{
     try{
-       
-        res.sendFile(path.resolve('static/main.html'));
+        firstName=req.session.user.firstName;
+        lastName=req.session.user.lastName;
+        profilePicture=req.session.user.profilePicture;
+        email = req.session.user.email;
+        res.render('dashboard/dashboard', {
+            layout: "dashboardMain",firstName: firstName,lastName: lastName,profilePicture: profilePicture, email:email});
     }catch(e){
         res.status(500).json({error:e});
     }
