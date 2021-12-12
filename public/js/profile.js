49a21b210470bbbd39f38919441ab32d2d41ff09
govindr15 <govindr15895@gmail.com>
2021-12-12 00:31:57 -0500

    adding progress bar in profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,13 +1,54 @@
 window.addEventListener('load', (event) => {
-    let firstName=document.getElementById("first_name").innerText;
-    let lastName=document.getElementById("last_name").innerText;
-    let scram = document.getElementById("scram").innerText;
+    let firstName=document.getElementById("firstName").innerText;
+    let lastName=document.getElementById("lastName").innerText;
+    let profilePicture = document.getElementById("profilePicture").innerText;
     console.log('page is fully loaded');
     let name=firstName+" "+lastName;
     document.getElementById("right_name").innerText=name;
     document.getElementById("p_name").innerText=name;
     
-    document.getElementById("img_profile").src=`../../public/uploads/${scram}`;
+    document.getElementById("img_profile").src=`../../public/uploads/${profilePicture}`;
+
+    let total_no_of_words=document.getElementById("noOfWords").innerText;
+    let words_yetToLearn=document.getElementById("noOfYetToLearnWords").innerText;
+    let words_learning=document.getElementById("noOfLearningWords").innerText;
+    let words_learnt=document.getElementById("noOfLearntWords").innerText;
+
+    let percentage1;
+    if(total_no_of_words==0 || words_learnt==0){
+        percentage1=0;
+    }else{
+        percentage1=words_learnt/total_no_of_words*100;
+        percentage1=Math.round(percentage1);
+    }
+    document.getElementById("progress_bar_1").style.width=percentage1+"%";
+    document.getElementById("perc1").innerText=percentage1+"%";
+    document.getElementById("wordCount1").innerText=words_learnt;
+    document.getElementById("wordTotal1").innerText=total_no_of_words;
+
+    let percentage2;
+    if(total_no_of_words==0 || words_yetToLearn==0){
+        percentage2=0;
+    }else{
+        percentage2=words_yetToLearn/total_no_of_words*100;
+        percentage2=Math.round(percentage2);
+    }
+    document.getElementById("progress_bar_2").style.width=percentage2+"%";
+    document.getElementById("perc2").innerText=percentage2+"%";
+    document.getElementById("wordCount2").innerText=words_yetToLearn;
+    document.getElementById("wordTotal2").innerText=total_no_of_words;
+
+    let percentage3;
+    if(total_no_of_words==0 || words_learning==0){
+        percentage3=0;
+    }else{
+        percentage3=words_learning/total_no_of_words*100;
+        percentage3=Math.round(percentage3);
+    }
+    document.getElementById("progress_bar_3").style.width=percentage3+"%";
+    document.getElementById("perc3").innerText=percentage3+"%";
+    document.getElementById("wordCount3").innerText=words_learning;
+    document.getElementById("wordTotal3").innerText=total_no_of_words;
 });
 
 
--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const router=express.Router();
-
+const data = require("../data");
+const userData = data.users;
+// const mcqData = data.mcq;
+const { getAll, getLearning, getYetToLearn, getLearnt } = data.words
 
 router.get('/', async(req,res)=>{
     try{
@@ -8,7 +11,12 @@ router.get('/', async(req,res)=>{
         lastName=req.session.user.lastName;
         profilePicture=req.session.user.profilePicture;
         email = req.session.user.email;
-        res.render('profile/profile', {title:"Profile",firstName: firstName,lastName: lastName, profilePicture: profilePicture, email:email});
+        let id = req.session.user._id;
+        const { wordList, yetToLearnWords, learningWords, learntWords } = await getAll(id);
+        res.render('profile/profile', {title:"Profile",firstName: firstName,lastName: lastName, profilePicture: profilePicture, email:email,
+        wordList: wordList, wordListLength: wordList.length, yetToLearnWords: yetToLearnWords, learningWords: learningWords,
+        learntWords: learntWords, yetToLearnWordsLength: yetToLearnWords.length, learningWordsLength: learningWords.length,
+        learntWordsLength: learntWords.length });
     }catch(e){
         res.status(500).json({error:e});
     }
